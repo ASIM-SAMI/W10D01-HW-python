904ca193bff16871fbd3c7c5c0bf109cb202b3e4
yasser9947 <yasser9946.1@gmail.com>
2021-01-31 07:15:27 +0300

    update

--- fibonacci/fib.py
+++ fibonacci/fib.py
@@ -1,3 +1,22 @@
 def fib(x):
-  # Your code here 
- 
+  if x == 0: 
+    return print(0)
+  elif x == 1: 
+    return print(1)
+  else: 
+    fn0 = 0
+    fn1 = 1 
+    count = 2
+    while True:
+      fn2 = fn1 + fn0
+      if count == x: 
+        return print(fn2)
+        break
+      else:
+        fn0 = fn1
+        fn1 = fn2
+        count = count + 1
+        continue
+  pass 
+
+fib(6)
